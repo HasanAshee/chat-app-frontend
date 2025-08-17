a78a204d7acd27eaf175a1c66b007c75730bc9ab
Hasan Carrizo <hasan.carrizo2002@gmail.com>
2025-08-17 17:55:47 -0300

    Add render url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'URL-DE-TU-BACKEND-EN-RENDER' // Dejamos esto pendiente por ahora
+  apiUrl: 'https://chat-app-backend-ra36.onrender.com' // Dejamos esto pendiente por ahora
 };
